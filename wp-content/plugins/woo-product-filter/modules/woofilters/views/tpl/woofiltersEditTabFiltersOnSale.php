eb31e3dde933008b96a7f49f949fbe785f1fe336
Pereyma Igor <igor.pereyma.ua@gmail.com>
2020-05-22 16:36:15 +0300

    add widgets

---
+++ wp-content/plugins/woo-product-filter/modules/woofilters/views/tpl/woofiltersEditTabFiltersOnSale.php
@@ -0,0 +1,52 @@
+<div class="row-settings-block">
+	<div class="settings-block-label col-xs-4 col-sm-3">
+		<?php esc_html_e('Show title label', 'woo-product-filter'); ?>
+		<i class="fa fa-question woobewoo-tooltip no-tooltip" title="<?php echo esc_attr__('Show title label', 'woo-product-filter'); ?>"></i>
+	</div>
+	<div class="settings-block-values col-xs-8 col-sm-9">
+		<div class="settings-value">
+			<?php 
+				HtmlWpf::selectbox('f_enable_title', array(
+					'options' => array('no' => 'No', 'yes_close' => 'Yes, show as close', 'yes_open' => 'Yes, show as opened'),
+					'attrs' => 'class="woobewoo-flat-input"'
+				));
+				?>
+		</div>
+	</div>
+</div>
+<div class="row-settings-block">
+	<div class="settings-block-label col-xs-4 col-sm-3">
+		<?php esc_html_e('Show on frontend as', 'woo-product-filter'); ?>
+	</div>
+	<div class="settings-block-values col-xs-8 col-sm-9">
+		<div class="settings-value">
+			<?php 
+				HtmlWpf::selectbox('f_frontend_type', array(
+					'options' => array('list' => 'Checkbox', 'switch' => 'Toggle Switch' . $labelPro),
+					'attrs' => 'class="woobewoo-flat-input"'
+				));
+				?>
+		</div>
+	</div>
+</div>
+<?php
+if ($isPro) {
+	DispatcherWpf::doAction('addEditTabFilters', 'partEditTabFiltersSwitchType');
+}
+?>
+<div class="row-settings-block">
+	<div class="settings-block-label col-xs-4 col-sm-3">
+		<?php esc_html_e('Checkbox label', 'woo-product-filter'); ?>
+	</div>
+	<div class="settings-block-values col-xs-8 col-sm-9">
+		<div class="settings-value">
+			<?php 
+				$labels = $this->getModel('woofilters')->getFilterLabels('OnSale');
+				HtmlWpf::text('f_checkbox_label', array(
+					'placeholder' => esc_attr($labels['onsale']),
+					'attrs' => 'class="woobewoo-flat-input"'
+				));
+				?>
+		</div>
+	</div>
+</div>
